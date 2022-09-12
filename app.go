f571794c436d75ef8b9ee18192ff24d9f049db63
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-09-12 15:45:36 -0300

    fix: refactor

--- app.go
+++ app.go
@@ -12,10 +12,8 @@ func main() {
 	driverManager := plc4go.NewPlcDriverManager()
 	drivers.RegisterModbusTcpDriver(driverManager)
 
-	// Get a connection to a remote PLC
 	crc := driverManager.GetConnection("modbus-tcp://192.168.23.30")
 
-	// Wait for the driver to connect (or not)
 	connectionResult := <-crc
 	if connectionResult.GetErr() != nil {
 		fmt.Printf("------Error connecting to PLC: %s", connectionResult.GetErr().Error())
@@ -23,31 +21,26 @@ func main() {
 	}
 	connection := connectionResult.GetConnection()
 
-	// Make sure the connection is closed at the end
 	defer connection.BlockingClose()
 
-	// Prepare a read-request
 	readRequest, err := connection.ReadRequestBuilder().
 		AddQuery("field", "holding-register:26:REAL").
 		Build()
 	if err != nil {
-		fmt.Printf("error preparing read-request: %s", connectionResult.GetErr().Error())
+		fmt.Printf("------Error preparing read-request: %s", connectionResult.GetErr().Error())
 		return
 	}
 
-	// Execute a read-request
 	rrc := readRequest.Execute()
 
-	// Wait for the response to finish
 	rrr := <-rrc
 	if rrr.GetErr() != nil {
-		fmt.Printf("error executing read-request: %s", rrr.GetErr().Error())
+		fmt.Printf("------Error executing read-request: %s", rrr.GetErr().Error())
 		return
 	}
 
-	// Do something with the response
 	if rrr.GetResponse().GetResponseCode("field") != model.PlcResponseCode_OK {
-		fmt.Printf("error an non-ok return code: %s", rrr.GetResponse().GetResponseCode("field").GetName())
+		fmt.Printf("------Error an non-ok return code: %s", rrr.GetResponse().GetResponseCode("field").GetName())
 		return
 	}
 
